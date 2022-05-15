8282c3c94ff5ffa75f5fe79178ef17d81f4d52c7
dexteresc <jonathan.jeding@gmail.com>
2022-05-15 23:01:36 +0200

    fix: remove validation from middleware

--- src/middleware/passport.middleware.ts
+++ src/middleware/passport.middleware.ts
@@ -1,64 +1,25 @@
 import { Strategy, ExtractJwt, StrategyOptions } from "passport-jwt";
 import { PassportStatic } from "passport";
-import { pbkdf2 } from "crypto";
-import { getUserByEmail, createUser } from "../controller/user.controller";
 import { User } from "../models/User";
 
-
-function initialize(passport: PassportStatic) {
+export default (passport: PassportStatic) => {
   const opts: StrategyOptions = {
-    jwtFromRequest: ExtractJwt.fromAuthHeader(),
-    secretOrKey: 'ligma-secret'
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET
   };
   passport.use(
     new Strategy(opts, (jwtPayload, done) => {
-      // Verify that id is a number
-      if (typeof jwtPayload.id !== "number") {
-        return done(null, false);
-      }
-
-      User.findOne({ where: { id: jwtPayload.id } })
-        .then((user) => {
-          if (user) {
-            return done(null, user);
-          }
-          return done(null, false);
-        })
+      User.findOne({ where: { id: jwtPayload.user.id } })
+        .then(
+          (user) => {
+            if (user) {
+              return done(null, user);
+            }
+            return done(null, false);
+          },
+          (err) => done(err, false)
+        )
         .catch((err) => done(err, false));
-        return false
-      }
-    ));
-}
-
-export const signUp = (email: string, password: string) => {
-  const user = getUserByEmail(email);
-  if (user) {
-    throw new Error("User already exists");
-  }
-  if (!user) { // Signup dags
-    pbkdf2(password, 'salt', 100000, 64, 'sha512', async (err, derivedKey) => {
-      if (err) {
-        throw err;
-      }
-      await createUser(email, derivedKey.toString('hex'));
-    });
-  }
-  return false
+    })
+  );
 };
-
-export const logIn = async (email: string, password: string) => {
-  const user = await getUserByEmail(email);
-  if (!user) {
-    throw new Error("User not found");
-  }
-  pbkdf2(password, 'salt', 100000, 64, 'sha512', async (err, derivedKey) => {
-    if (err) {
-      throw err;
-    }
-    if (derivedKey.toString('hex') === user.password) {
-      return user;
-    }
-    throw new Error("Invalid password");
-  });
-  return null;
-}
