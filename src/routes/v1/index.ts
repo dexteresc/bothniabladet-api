cf8cf38aeaaed06c059151adc34a7c70f4bdb3d0
dexteresc <jonathan.jeding@gmail.com>
2022-05-15 23:02:34 +0200

    fix: add middleware to routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,14 +1,28 @@
 import { Router } from "express";
+import passport = require("passport");
 import photoRouter from "./photo";
 import userRouter from "./user";
 import categoryRouter from "./category";
 import authRouter from "./auth";
 
 const router = Router();
+// Add authenticate middleware to all routes except auth
 
-router.use("/photo", photoRouter);
-router.use("/user", userRouter);
-router.use("/category", categoryRouter);
+router.use(
+  "/photo",
+  passport.authenticate("jwt", { session: false }),
+  photoRouter
+);
+router.use(
+  "/user",
+  passport.authenticate("jwt", { session: false }),
+  userRouter
+);
+router.use(
+  "/category",
+  passport.authenticate("jwt", { session: false }),
+  categoryRouter
+);
 router.use("/auth", authRouter);
 
 export default router;
